b41d4b944d4ebd6e00b6c7901bdeca50c66f9310
Jules Vannier <julesvannier44@gmail.com>
2022-12-18 14:24:39 +0100

    Event base class

--- JulesEngine/src/JulesEngine/Core.h
+++ JulesEngine/src/JulesEngine/Core.h
@@ -9,3 +9,5 @@
 #else
     #error JulesEngine only support Windows
 #endif
+
+#define BIT(x) (1<<x)
--- premake5.lua
+++ premake5.lua
@@ -27,6 +27,7 @@ project "JulesEngine"
 
     includedirs
     {
+	"%{prj.name}/src",
         "%{prj.name}/vendor/spdlog/include"
     }
 
