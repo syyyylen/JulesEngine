6394e3582b2fd53165cc4ead03c360184d912afb
Jules Vannier <julesvannier44@gmail.com>
2023-03-18 19:13:59 +0100

    glm maths lib

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "JulesEngine/vendor/GLFW/include"
 IncludeDir["Glad"] = "JulesEngine/vendor/Glad/include"
 IncludeDir["ImGui"] = "JulesEngine/vendor/imgui"
+IncludeDir["glm"] = "JulesEngine/vendor/glm"
 
 include "JulesEngine/vendor/GLFW"
 include "JulesEngine/vendor/Glad"
@@ -34,7 +35,9 @@ project "JulesEngine"
     files
     {
         "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/glm/glm/**.hpp",
+        "%{prj.name}/vendor/glm/glm/**.inl"
     }
 
     includedirs
@@ -43,7 +46,8 @@ project "JulesEngine"
         "%{prj.name}/vendor/spdlog/include", 
         "%{IncludeDir.GLFW}",
         "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}"
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.glm}"
     }
 
     links
@@ -103,7 +107,8 @@ project "Sandbox"
     includedirs
     {
         "JulesEngine/vendor/spdlog/include",
-        "JulesEngine/src"
+        "JulesEngine/src",
+        "%{IncludeDir.glm}"
     }
 
     links
