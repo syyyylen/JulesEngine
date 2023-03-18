a0075cb8e8940e7874580df37bea657869c46dff
Jules Vannier <julesvannier44@gmail.com>
2023-03-18 16:08:21 +0100

    ImGui Events

--- JulesEngine/src/JulesEngine/Events/KeyEvent.h
+++ JulesEngine/src/JulesEngine/Events/KeyEvent.h
@@ -50,4 +50,19 @@ namespace JulesEngine
 
         EVENT_CLASS_TYPE(KeyReleased)
     };
+
+    class JULESENGINE_API KeyTypedEvent : public KeyEvent
+    {
+    public :
+        KeyTypedEvent(int keycode) : KeyEvent(keycode){}
+
+        std::string ToString() const override
+        {
+            std::stringstream ss;
+            ss << "KeyTypedEvent: " << m_KeyCode;
+            return ss.str();
+        }
+
+        EVENT_CLASS_TYPE(KeyTyped)
+    };
 }
--- JulesEngine/src/JulesEngine/ImGui/ImGuiLayer.h
+++ JulesEngine/src/JulesEngine/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 ﻿#pragma once
 
 #include "JulesEngine/Layer.h"
+#include "JulesEngine/Events/ApplicationEvent.h"
+#include "JulesEngine/Events/MouseEvent.h"
+#include "JulesEngine/Events/KeyEvent.h"
 
 namespace JulesEngine
 {
@@ -14,6 +17,16 @@ namespace JulesEngine
         void OnDetach();
         void OnUpdate();
         void OnEvent(Event& event);
+
+    private:
+        bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+        bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+        bool OnMouseMovedEvent(MouseMovedEvent& e);
+        bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+        bool OnKeyPressedEvent(KeyPressedEvent& e);
+        bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+        bool OnKeyTypedEvent(KeyTypedEvent& e);
+        bool OnWindowResizedEvent(WindowResizeEvent& e);
 
     private:
         float m_Time = 0.0f;
